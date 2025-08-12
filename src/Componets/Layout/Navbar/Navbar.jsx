34f0aed0600cfd176c09449b93149b59ea01ea4a
nirvanaemploye <nirvanaemploye@gmail.com>
2025-08-12 14:36:11 +0530

    Update Navbar.jsx

--- src/Componets/Layout/Navbar/Navbar.jsx
+++ src/Componets/Layout/Navbar/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = ({ className = "" }) => {
           <Link
             to="/"
             className={`font-bold flex items-baseline ${isHomePage ? "text-black" : "text-white"
-              } text-lg sm:text-2xl lg:text-2xl tracking-wide`}
+              } text-lg md:text-2xl xl:text-[28px] tracking-wide`}
           >
             <img
               src={logo}
