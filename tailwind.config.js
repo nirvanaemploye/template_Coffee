85038c783ccba3d06554501c023b27141ae023f8
nirvanaemploye <nirvanaemploye@gmail.com>
2025-08-14 22:11:10 +0530

    font erroe on 1408

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        custom: ["MyCustomFont", "sans-serif"],
+        custom: ["JamGrotesque", "sans-serif"],
         openSans: ["Open Sans", "sans-serif"],
         cursive: ["Pacifico", "serif"],
       },
